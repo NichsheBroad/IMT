46c6a8c285ca9f8ca960b9127a4a925ba8d19169
tmshnk0 <tmshnk0@yandex.ru>
2025-05-18 17:16:20 +0500

    minor edits

--- cmd/my_app/main.go
+++ cmd/my_app/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"IMT/internal/servise"
+	s "IMT/internal/servise"
 )
 
 func main() {
-	userHeight := servise.GetPositiveInput("Введите рост в метрах (пример: 1.75): ")
-	userKg := servise.GetPositiveInput("Введите вес в килограммах (пример: 68.5): ")
+	userHeight := s.GetPositiveInput("Введите рост в метрах (пример: 1.75): \n")
+	userKg := s.GetPositiveInput("Введите вес в килограммах (пример: 68.5): \n")
 
-	imt := servise.CalculateIMT(userHeight, userKg)
-	servise.ShowIMTMessage(imt)
+	imt := s.CalculateIMT(userHeight, userKg)
+	s.ShowIMTMessage(imt)
 }
